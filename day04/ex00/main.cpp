ad7de8a041db31911cac383d033c194239e15ac0
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2022-04-30 11:06:33 +0200

    Edit day04/ex00

--- day04/ex00/main.cpp
+++ day04/ex00/main.cpp
@@ -4,10 +4,15 @@
 
 int main()
 {
-Sorcerer robert("Robert", "the Magnificent");
-Victim jim("Jimmy");
-Peon joe("Joe");
-std::cout << robert << jim << joe;
-robert.polymorph(jim);
-robert.polymorph(joe);
-return 0; }
+	Sorcerer robert("Robert", "the Magnificent");
+
+	Victim jim("Jimmy");
+	Peon joe("Joe");
+
+	std::cout << robert << jim << joe;
+
+	robert.polymorph(jim);
+	robert.polymorph(joe);
+
+	return 0;
+}
